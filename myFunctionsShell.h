066fa744f2fed9a9670520b1ab9e3376123038c9
rongart <rongartenbank@gmail.com>
2025-01-31 14:54:49 +0200

    added documentation to the functions

--- myFunctionsShell.h
+++ myFunctionsShell.h
@@ -6,9 +6,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
+/**
+ * gets input from the user
+ *
+ *this function activates a loop that expects the user to enter input, the loop ends when the 'enter' key is pressed
+ * 
+ * @return a char array of the user input
+ *
+ */
 char* inputFromUser();
+
+
+/**
+ * Prints location of this program
+ *
+ * Prints the user name and computer name and then the current path the program is run on 
+ */
 void getLocation();
+
+
+/**
+ * Splits a string into individual tokens based on whitespace.
+ *
+ * This function takes a string as input and tokenizes it based on whitespace
+ * characters (space, tab, newline). It dynamically allocates memory for an
+ * array of strings to store the tokens. The caller is responsible for freeing
+ * the memory allocated for the array and its elements when it's no longer needed.
+ *
+ * @param input A pointer to the string to be split.
+ *
+ * @return A dynamically allocated array of strings (char**) containing the tokens.
+ *         The last element of the array is NULL. Returns NULL if input is NULL
+ *         or if memory allocation fails.
+ *
+ * @note The input string will be modified (tokenized) by this function.
+ *       Make sure to use a copy of the original string if preservation
+ *       of the original string is required.
+ *
+ * @warning The caller is responsible for freeing the memory allocated for
+ *          the returned array and its elements using free().
+ */
 char **splitArguments(char* );
 void systemCall(char** arguments);
 void logout();
